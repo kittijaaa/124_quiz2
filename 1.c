2a448df464ca8e0b9f0a36e75da7e631bbabf607
kittijaaa <kittipat@ku.th>
2021-03-25 17:56:27 +0700

    1

--- 1.c
+++ 1.c
@@ -1,25 +1,28 @@
 #include<stdio.h>
 int main() {
-    long n=0,i,j=1,x=2,k=8,l=0,m=0 ;
-    for(;n<=100000000;){
-        scanf("%d",&n) ;
+    long n=1,i,j=1,x=2,k=8,l,m,y=0 ;
+    if(n>=1&&n<=1000000)
+    scanf("%ld",&n) ;
         m=0 ;l=0 ;
             for(j=1;j<=n;j++){
-                l++ ;
+                l++ ; k=8;x=2;
                 for(;j>=k;x++,k=x*x*x){
                   if(j%k==0){
                         m++ ;
                         break ;
                   }
                 }
+            if(j==n){
+                for(;j>=k;x++,k=x*x*x){
+                  if(j%k==0){
+                        printf("Not Cube Free") ;
+                        y=1;
+                        break ;
+                  }
+                }
+                if(y!=1)
+                    printf("%ld",l-m) ;
+                    }
             }
-            if(l==n){
-                printf("%d",l) ;
-            }else if(l+m==n){
-                printf("%d",l-m) ;
-
-            }
-
-    }
     return 0 ;
 }
